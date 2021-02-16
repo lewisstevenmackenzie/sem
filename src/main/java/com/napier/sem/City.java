e51ac8e38aa88ac034fb9621aac06b8050880032
Lewis Mackenzie <lewisstevenmackenzie@gmail.com>
2021-02-16 16:04:09 +0000

    created Country,language and city classes

---
+++ src/main/java/com/napier/sem/City.java
@@ -0,0 +1,32 @@
+package com.napier.sem;
+
+/**
+ * Represents a city
+ */
+public class City {
+
+    /**
+     * the city ID
+     */
+    public int ID;
+
+    /**
+     * the Cities name
+     */
+    public String name;
+
+    /**
+     * the country code where the city resides
+     */
+    public String countryCode;
+
+    /**
+     * the District the city is located
+     */
+    public String district;
+
+    /**
+     * the cities population
+     */
+    public int population;
+}
---
+++ src/main/java/com/napier/sem/CountryLanguage.java
@@ -0,0 +1,23 @@
+package com.napier.sem;
+
+public class CountryLanguage {
+    /**
+     * the country Code
+     */
+    public String countryCode;
+
+    /**
+     * the countries main language
+     */
+    public String language;
+
+    /**
+     * NOT REALLY SURE
+     */
+    public enum isOfficial{};
+
+    /**
+     * percentage of people that speak it
+     */
+    public double percentage;
+}
